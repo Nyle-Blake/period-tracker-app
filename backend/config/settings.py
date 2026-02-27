a062cbbc932f6f11926e4cad3e1b6d145a53c5c1
Nyle-Blake <Nyleblake1@gmail.com>
2026-02-27 13:38:22 +0000

    fix: fix getenv in allowed hosts in settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-!*@e-@w^9!5ypr5^=9^yv1k&^je8e@^rv*t$x=007(i$@#!rff
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', getenv('TEST_IP')] # Dotenv does not currently work
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', getenv('TEST_IP', '')]
 
 # Application definition
 
